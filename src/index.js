8033a3aeea6d85f5953b8109f5107f68843df44f
Bo HOU <bhoustudio@gmail.com>
2016-05-17 00:31:24 +0200

    init commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  /**
+   * Create an error with status code and source
+   * @param  {number} code     status code
+   * @param  {string | object} error object or error message
+   * @param  {object} source   error source
+   * @return {Error}          Error object with statusCode and source
+   */
+  error(code, errOrMsg, source) {
+    var error = errOrMsg;
+    if (typeof errOrMsg == 'string') {
+      error = new Error(errOrMsg);
+    }
+
+    error.statusCode = code;
+
+    if (source) error.source = source;
+
+    error.toString = function() {
+      return `[${error.statusCode}]:${error.message}`;
+    };
+
+    error.toResult = function() {
+      return error.source.new(error);
+    }
+
+    return error;
+  }
+}
